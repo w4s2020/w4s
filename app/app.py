ee319fa3b3268c103f78a77f4c432189b4d5c99f
Blazar1000 <Gianluca.Russo@tatacommunications.com>
2020-02-17 23:47:04 +0000

    added first API route

--- app/app.py
+++ app/app.py
@@ -18,7 +18,41 @@ posts = [
 
 ]
 
-@app.route('/')
+@app.route('/food/feasible/recipes', methods=['GET'])
+def find_recipes():
+    recipes_filtered = []
+    feasible_recipes = {}
+    feasible_recipes_length = []
+    feasible_recipes_title = []
+    url_on_file = open("URL.txt", r)
+    url = url_on_file.read()
+    number_of_recipes = request.args.get('number')
+    available_ingredients = request.args.get('ingredients')
+    querystring = {"number": number_of_recipes,"ranking": "1", "ignorePantry": "true", "ingredients": available_ingredients, "apiKey":"aaaa"}
+
+    response  = requests.get(url, params=querystring)
+    recipes_list_length = len(response.json())
+    recipes = Response(response, status=200, mimetype='application/json')
+
+    for x in range(0, recipes_list_length):
+        if ((response.json()[x]["missedIngredientCount"]) < 3):
+            recipes_filtered.append(response.json()[x])
+    feasible_recipes_length = len(recipes_filtered)
+    recipes_missing_ingredients = {}
+
+    for x in range(0, feasible_recipes_length):
+
+         feasible_recipes_title.append(recipes_filtered[x]["title"])
+         missing_ingredients = []
+         for y in range(0, recipes_filtered[x]["missedIngredientCount"]):
+             missing_ingredients.append(recipes_filtered[x]["missedIngredients"][y]["name"])
+         item = recipes_filtered[x]["title"]
+         recipes_missing_ingredients[item] = missing_ingredients
+
+    return render_template('recipes.html', data = recipes_missing_ingredients)
+
+
+
 @app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
@@ -26,6 +60,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title ='About')
+
+
+
+
 
 @app.route('/createtable')
 def createtable():
