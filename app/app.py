46703869dd8762e66cdbabd01338d4069dc85c87
Blazar1000 <Gianluca.Russo@tatacommunications.com>
2020-02-20 00:52:30 +0000

    Modified API

--- app/app.py
+++ app/app.py
@@ -44,51 +44,57 @@ posts = [
 
 @app.route('/food/feasible/recipes', methods=['GET','POST'])
 def find_recipes():
-    recipes_filtered = []
-    feasible_recipes = {}
-    feasible_recipes_length = []
-    feasible_recipes_title = []
+    if request.method == "POST":
 
-    url = "https://api.spoonacular.com/recipes/findByIngredients"
-    f = open(API.txt, r)
-    API = f.read()
-    number_of_recipes = request.args.get('number')
-    available_ingredients = request.args.get('ingredients')
-    querystring = {"number": number_of_recipes,"ranking": "1", "ignorePantry": "true", "ingredients": available_ingredients, "apiKey":API}
+        recipes_filtered = []
+        feasible_recipes = {}
+        feasible_recipes_length = []
+        feasible_recipes_title = []
 
-    response  = requests.get(url, params=querystring)
-    recipes_list_length = len(response.json())
-    recipes = Response(response, status=200, mimetype='application/json')
+        url = "https://api.spoonacular.com/recipes/findByIngredients"
+        f = open('/home/ec2-user/API.txt', 'r')
+        API = (f.read()).rstrip()
+        number_of_recipes = 10
 
-    for x in range(0, recipes_list_length):
-        if ((response.json()[x]["missedIngredientCount"]) < 3):
+        available_ingredients = request.form['ingredients']
+        kitchen = request.form['kitchen']
+        course = request.form['course']
 
-            recipes_filtered.append(response.json()[x])
-    feasible_recipes_length = len(recipes_filtered)
-    recipes_missing_ingredients = {}
-    recipes_used_ingredients = {}
-    recipe_total_ingredients = {}
+        querystring = {"number": number_of_recipes,"ranking": "1", "ignorePantry": "true", "ingredients": available_ingredients, "apiKey": API}
+        response  = requests.get(url, params=querystring)
+        recipes_list_length = len(response.json())
+        recipes = Response(response, status=200, mimetype='application/json')
 
-    for x in range(0, feasible_recipes_length):
+        for x in range(0, recipes_list_length):
+            if ((response.json()[x]["missedIngredientCount"]) < 3):
 
-         feasible_recipes_title.append(recipes_filtered[x]["title"])
-         total_ingredients = []
-         missing_ingredients = []
-         used_ingredients = []
-         item = recipes_filtered[x]["title"]
-         for y in range(0, recipes_filtered[x]["missedIngredientCount"]):
-             missing_ingredients.append(recipes_filtered[x]["missedIngredients"][y]["name"])
+                recipes_filtered.append(response.json()[x])
+                feasible_recipes_length = len(recipes_filtered)
+                recipes_missing_ingredients = {}
+                recipes_used_ingredients = {}
+                recipe_total_ingredients = {}
 
+        for x in range(0, feasible_recipes_length):
 
-         for z in range(0, recipes_filtered[x]["usedIngredientCount"]):
-             used_ingredients.append(recipes_filtered[x]["usedIngredients"][z]["name"])
-
-         total_ingredients.append(missing_ingredients)
-         total_ingredients.append(used_ingredients)
-         recipe_total_ingredients[item] = total_ingredients
+            feasible_recipes_title.append(recipes_filtered[x]["title"])
+            total_ingredients = []
+            missing_ingredients = []
+            used_ingredients = []
+            item = recipes_filtered[x]["title"]
+            for y in range(0, recipes_filtered[x]["missedIngredientCount"]):
+                missing_ingredients.append(recipes_filtered[x]["missedIngredients"][y]["name"])
 
 
-    return render_template('recipes.html', data = recipe_total_ingredients)
+            for z in range(0, recipes_filtered[x]["usedIngredientCount"]):
+                used_ingredients.append(recipes_filtered[x]["usedIngredients"][z]["name"])
+
+            total_ingredients.append(missing_ingredients)
+            total_ingredients.append(used_ingredients)
+            recipe_total_ingredients[item] = total_ingredients
+
+
+        return render_template('recipes.html', data = recipe_total_ingredients)
+    return render_template('find_recipes.html')
 
 
 @app.route('/home')
